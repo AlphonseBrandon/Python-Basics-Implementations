02572580e9226b65910180cbb63875b465f97f2f
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-16 21:19:23 +0100

    first item in array

--- loops.py
+++ loops.py
@@ -6,7 +6,13 @@ people = ['Brandon', 'Alphonse', 'John', 'Mary']
 
 for person in people:
     print(f'current person: {person}')
-    
+
+# Break
+for person in people:
+    if person == 'Brandon':
+        break
+    print(f'current person: {person}')
+
 
 
 # While loops execute a set of statements as long as a condition is true.
