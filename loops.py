8eaf02009872db5ef3835d16d80e8715691da2d1
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-16 21:13:38 +0100

    simple for loop

--- loops.py
+++ loops.py
@@ -1,5 +1,12 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['Brandon', 'Alphonse', 'John', 'Mary']
+
+#Simple 'for' loop
+
+for person in people:
+    print(f'current person: {person}')
+    
 
 
 # While loops execute a set of statements as long as a condition is true.
