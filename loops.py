4800e5b9768dba314472199adeeac70c29ccd6f9
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-16 21:28:06 +0100

    the range function looping through an array

--- loops.py
+++ loops.py
@@ -19,6 +19,11 @@ for person in person:
         continue
     print(f'current person: {person}')
 
+# range
+for i in range(len(people)):
+    print(people[i])
+    
+
 
 
 # While loops execute a set of statements as long as a condition is true.
