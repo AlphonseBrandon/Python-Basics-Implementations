4ac24711e4bed6c39fa341e69f47a5bb4cb5c1e9
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-17 06:50:27 +0100

    comparing two conditions with if else satements

--- conditionals.py
+++ conditionals.py
@@ -5,13 +5,17 @@ y = 20
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+# Simple if
 if x > y:
     print(f'{x} is greater than {y}')
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
-
-
+# if/else
+if x > y:
+    print(f'{x} is greater than {y}')
+else:
+    print(f'{y} is greater than {x}')
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
