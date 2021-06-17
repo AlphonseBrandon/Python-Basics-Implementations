a4b179c274a1bebbf73f2fd5ea7c6d03c66c0b26
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-17 06:59:39 +0100

    logical operator "and"

--- conditionals.py
+++ conditionals.py
@@ -9,13 +9,24 @@ y = 20
 if x > y:
     print(f'{x} is greater than {y}')
 
-# Logical operators (and, or, not) - Used to combine conditional statements
-
 # if/else
 if x > y:
     print(f'{x} is greater than {y}')
 else:
     print(f'{y} is greater than {x}')
+
+#elif
+if x > y:
+    print(f'{x} is equall to {y}')
+elif x == y:
+    print (f'{x} is equal to {y}')
+else:
+    print(f'{y} is greater than {x}')
+# Logical operators (and, or, not) - Used to combine conditional statements
+# and
+if x > 2 and x= 10:
+    print(f'{x} is greater than 2 and less than 10')
+    
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
