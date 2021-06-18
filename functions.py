b3d8c499c92e26d353a0055331baed80e5dacaa3
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-18 08:05:54 +0100

    the lambda function

--- functions.py
+++ functions.py
@@ -13,3 +13,6 @@ def getSum(num1, num2):
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+getSum = lambda num1, num2: num1 + num2
+
+print(getSum(10, 3))
