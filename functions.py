a01bb754fcc4f592de14df2e9de6fae068d6ed96
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-18 08:01:10 +0100

    Return values from function

--- functions.py
+++ functions.py
@@ -5,6 +5,10 @@
 def sayHello(name='Sam'):
     print(f'Hello {name}')
 
+# Return values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
