4986ef05111b6c0843d83e9125048ac75d1d8fdd
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-19 19:42:55 +0100

    Core python modules

--- modules.py
+++ modules.py
@@ -1,1 +1,7 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# Core modules
+import datetime
+from datetime import date
+import time
+from time import time
