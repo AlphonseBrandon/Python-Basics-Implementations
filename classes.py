7364dd26e5a8b46598905c98647d1739fe3d4152
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-20 12:06:25 +0100

    Classes in python init

---
+++ classes.py
@@ -0,0 +1,10 @@
+# A class is like a blueprint for creating objects. An object has properties and methods(functions) associated with it. Almost everything in Python is an object
+
+# Create class
+class User:
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = Alphonse
+        self.email = alphonse.brand@gmail.com
+        self.age = age
+        
--- functions.py
+++ functions.py
@@ -11,7 +11,7 @@ def getSum(num1, num2):
     return total
 
 # A lambda function is a small anonymous function.
-# A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
+# A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions 
 
 getSum = lambda num1, num2: num1 + num2
 
