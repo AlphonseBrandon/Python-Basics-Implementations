953e891a8871cbf199d279df45c3883462dc8305
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-22 21:37:52 +0100

    F-strings

--- strings.py
+++ strings.py
@@ -5,12 +5,15 @@ age = 105
 
 
 # Concatenate
-print('Hello, my name is ' + name + 'and i am' + str(age))
+print('Hello, my name is ' + name +
+' ' + 'and i am' +
+' ' + str(age))
 
 # String Formatting
 
 # Arguments by position
 print('My name is {name} and i am {age}'.format(name=name, age=age))
 
-
+# F -strings
+print(f'Hello, my name is {name} and i am {age}')
 # String Methods
