9e2c7f90d48391690199c25ffe79f413a175ec26
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-22 21:30:26 +0100

    Concatenating stings and variables

--- strings.py
+++ strings.py
@@ -1,5 +1,11 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Alphonse'
+age = 105
+
+
+# Concatenate
+print('Hello, my name is ' + name + 'and i am' + str(age))
 
 # String Formatting
 
