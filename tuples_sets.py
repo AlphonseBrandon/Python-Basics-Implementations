feb41bad0c143bbaade6f827c1cb1e3e287adcd4
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-24 19:51:57 +0100

    initialized a tuple

--- tuples_sets.py
+++ tuples_sets.py
@@ -1,5 +1,7 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- 
+
+# Create turple
+fruits = ('Apples', 'Oranges', 'Grapes') 
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
