6c834fe34b4960b72884fd7cc7918f1808082b4f
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-24 19:56:36 +0100

    Using the turple constructor

--- tuples_sets.py
+++ tuples_sets.py
@@ -2,6 +2,11 @@
 
 # Create turple
 fruits = ('Apples', 'Oranges', 'Grapes') 
+print(fruits)
+
+# Using a constructor
+fruits2 = tuple('Apples Orange')
+print(fruits2)
 
 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
