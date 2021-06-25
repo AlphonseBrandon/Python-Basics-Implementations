545f7d024c37920ca628d8cf766383894460ba80
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-25 19:33:42 +0100

    Created a dictionary person

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,9 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+# Create dict
+person = {
+    'first_name': 'Alphonse',
+    'last_name': 'Brandon'
+    'Age': 30
+}
