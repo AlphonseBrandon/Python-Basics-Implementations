e9b84478f01cc69872aef55bc51d4b8c7ba1e3cf
AlphonseBrandon <alphonse.brand@gmail.com>
2021-06-29 21:35:38 +0100

    created user json

--- py_json.py
+++ py_json.py
@@ -1,1 +1,7 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+
+import json
+
+# Sample JSON
+
+userJSON = '{"first_name": "Alphonse", "last_name": "Brandon", "Age": 40}'
