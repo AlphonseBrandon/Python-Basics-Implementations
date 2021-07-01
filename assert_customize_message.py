cebffeb7aacf347f63561ce0b190cc3bafd1b036
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-01 21:02:07 +0100

    function init

--- assert_customize_message.py
+++ assert_customize_message.py
@@ -1,9 +1,2 @@
 def division(num1: int, num2: int):
-    assert num2 != 0, "“num2 must be different from 0"
-    return num1 / num2
-
-
-division(2, 0)
-""" 
-AssertionError: “num2 must be different from 0
-"""
+    
