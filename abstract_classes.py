757ed0349f7250501c76cb464fc6e11d195b216f
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-02 22:59:36 +0100

    @abstractmethod

--- abstract_classes.py
+++ abstract_classes.py
@@ -1,7 +1,13 @@
-from abc import ABC 
+from abc import ABC, abstractmethod 
 
 class Animal(ABC):
 
     def __init__(self, name: str):
         self.name = name
         super().__init()
+
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+    
