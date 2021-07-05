46218d322530e4510c319d203585deb550ca1dd5
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-05 22:54:07 +0100

    while not end of game, continue looping

--- Hangman_todo2.py
+++ Hangman_todo2.py
@@ -14,10 +14,16 @@ for _ in range(word_lenth):
     display += "_"
 print(display)
 
-guess = input("Guess a later").lower()
-for position in range(len(chosen_word)):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a later").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
     
-print(display)
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
