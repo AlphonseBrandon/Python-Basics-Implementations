5027ff0633b731e223540369af2f5fdbce898deb
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-05 22:24:30 +0100

    created an empty list

--- Hangman_todo2.py
+++ Hangman_todo2.py
@@ -1,2 +1,12 @@
 import random
 word_list = ["ardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+display = []
+
+for letter in chosen_word:
+    display += "_"
+
+guess = input("Guess a later").lower()
+# testing
+print(f'the solution is {chosen_word}')
