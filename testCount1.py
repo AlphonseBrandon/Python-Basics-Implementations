3dfaba57b454736d3cd21c48934b37adf3cc15d8
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-07 23:30:05 +0100

    looping through the list

--- testCount1.py
+++ testCount1.py
@@ -5,5 +5,9 @@ char_list = ["1", "2", "3"]
 custom_char = []
 
 for char in char_list:
-    custom_char[char_list] = 1
+    for char not in custom_char:
+        custom_char[char] = 1
+    else:
+        custom_char[char] += 1
+
 
