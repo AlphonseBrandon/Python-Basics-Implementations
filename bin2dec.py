ca9e1a5c15b7c951cdbd7c3ea44f82a2abb1dbc1
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-09 20:37:05 +0100

    bug fixed

--- bin2dec.py
+++ bin2dec.py
@@ -1,3 +1,12 @@
 # convert from dec to bin
 
 n = input(" enter a number to be converted")
+
+number = int(n)
+bin = []
+while number > 1:
+    remainder = number % 2
+    
+    
+print(bin[remainder])
+
