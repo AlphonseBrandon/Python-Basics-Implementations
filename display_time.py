33c2392f36f347df7206a24eaabc5bc94e5fb784
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-14 21:58:12 +0100

    computing time given in secs to hours, mins and secs

--- display_time.py
+++ display_time.py
@@ -12,6 +12,6 @@ minutes = seconds_remaining // 60
 
 seconds_left = seconds_remaining % 60
 
-print(f"hrs: {hours} mins: {minutes} secs: {seconds_left}")
+print(f"hrs: {hours} \n mins: {minutes} \nsecs: {seconds_left}")
 
 
