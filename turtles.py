f2221385964a77f81439ffc9e13e4909551f083e
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-15 20:45:25 +0100

    move left

--- turtles.py
+++ turtles.py
@@ -9,3 +9,4 @@ window = turtle.Screen() # Create a playground for turtles
 alex = turtle.Turtle() # create a turtle asign to alex
 
 alex.forward(50) # telling alex to move by 50 units forward
+alex.left(90) # move left 
