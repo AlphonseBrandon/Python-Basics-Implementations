c3d2ba70b3a842e0844ad98aff934bb8c3072940
AlphonseBrandon <alphonse.brand@gmail.com>
2021-07-15 20:47:15 +0100

    Wait for the user to close main loop

--- turtles.py
+++ turtles.py
@@ -10,3 +10,6 @@ alex = turtle.Turtle() # create a turtle asign to alex
 
 alex.forward(50) # telling alex to move by 50 units forward
 alex.left(90) # move left 
+alex.forward(50)
+
+window.mainloop() # Wait for the user to close main loop
