669b220e1f70ad7b18c3a0e61779a228a4362cc2
AlphonseBrandon <alphonse.brand@gmail.com>
2022-04-21 22:56:42 +0100

    Updated animal list

--- lists.py
+++ lists.py
@@ -3,7 +3,7 @@
 # Create list
 numbers = [1, 2, 3, 4, 5]
 fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
-animals = ['Dog', 'Cat', 'Goat']
+animals = ['Dog', 'Cat', 'Goat', 'Lion']
 
 
 # Using a constructor
